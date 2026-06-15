2a3ab2bfd623059dedd62b93268fd9d5463dd3cd
Kael Daniel <danielko552@mail.sandhills.edu>
2026-06-15 03:44:21 +0000

    sigh

---
+++ src/labs/examples/helloWorld.java
@@ -0,0 +1,15 @@
+package labs.examples;
+
+/*
+@Author: Kael Daniel
+@Date: 6/14/2026
+@Purpose: my second java program (pain, just pain)
+*/
+
+public class helloWorld //public definition class
+{
+    public static void main(String[] args) //starts the code, string array of arguments
+    {
+        System.out.println("Hello World"); //prints the line "Hello World"
+    }
+}
