0119f436e31beade323ad4eb0a870b7713b45776
jadTalbert <talbertj@sandhills.edu>
2026-07-19 23:06:36 -0400

    a real pain to recreat while not being an exact copy

--- src/labs/examples/ArrayOptions/Logger.java
+++ src/labs/examples/ArrayOptions/Logger.java
@@ -1,39 +1,48 @@
-package labs.examples.ArrayOptions;
+﻿package labs.examples.ArrayOptions;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.List;
 
+public class Logger {
 
+    private static final String File_Path = "C:\\Users\\Kael\\CSC_151_Kael_Daniel_2nd\\src\\labs\\examples\\ArrayOptions\\Files\\";
+    private static final String Error_Logger_File = File_Path + "api_error.log";
 
-public class Logger{
-
-    private static final String File_Path = "T:\\CSC_151_Kael_Daniel\\src\\labs\\example\\fileOperations\\";
-    private static final String Error_Logger_File = File_Path + "logs\\csv_error.log";
-    private static final String Api_Logger_File = File_Path + "logs\\api_error.log";
-    private static final String Http_Logger_File = File_Path + "logs\\http_access.log";
-
-    public static void main(BufferedReader reader) throws IOException {
-        openErrorLog();
-    } 
-    
-    public static BufferedReader openErrorLog() throws FileNotFoundException, IOException{
-        File csv_error = new File(Error_Logger_File);
-        return new BufferedReader(new FileReader(csv_error));
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        BufferedReader reader = openErrorLog();
+        countErrors(reader);
+        reader.close();
     }
-    public static void getCountOfErrorTypes(BufferedReader reader) throws IOException {
-        List<String> logLevels = new ArrayList<>(); // creates a new list to store the log levels found in the error log file
-        String error_Line;
-        while ((error_Line = reader.readLine()) != null) { // reads each line of the error log file until reaching the end
-            String[] error_count = error_Line.split(" ");
-            if (error_count.length > 2) { // checks if the line has at least 3 splits
-                logLevels.add(error_count[2]); // adds the 3rd section to the array list, which would be the error types
+
+    public static BufferedReader openErrorLog() throws FileNotFoundException, IOException {
+        File file = new File(Error_Logger_File);
+        return new BufferedReader(new FileReader(file));
+    }
+
+    public static void countErrors(BufferedReader reader) throws IOException {
+        int[] counts = new int[3];
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            String[] parts = line.split(" ");
+            if (parts.length > 2) {
+                String type = parts[2];
+
+                if (type.equals("[ERROR]")) {
+                    counts[0]++;
+                } else if (type.equals("[WARN]")) {
+                    counts[1]++;
+                } else if (type.equals("[INFO]")) {
+                    counts[2]++;
+                }
             }
         }
+
+        System.out.println("[ERROR]: " + counts[0]);
+        System.out.println("[WARN]: " + counts[1]);
+        System.out.println("[INFO]: " + counts[2]);
     }
 }
